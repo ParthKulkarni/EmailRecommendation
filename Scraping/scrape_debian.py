811b806edd89d8b0ca87dc75238d2a3276f993bc
vikas kodag <vikaskodag2@gmail.com>
2019-01-04 10:19:26 +0530

    new scraper for debian

--- Scraping/scrape_debian.py
+++ Scraping/scrape_debian.py
@@ -3,19 +3,47 @@ import requests
 from pprint import pprint
 import re
 
-# URL of page to be scraped
-page = requests.get('https://lists.debian.org/debian-devel/2018/01/threads.html')
-soup = BeautifulSoup(page.text, 'html.parser')
+import requests
+from bs4 import BeautifulSoup
+import urllib
+ 
+url = 'https://lists.debian.org/debian-devel/'
+year = 2019
+month = 0
+url1 = 'threads.html'
 
-# Get all links on the page and store in a list
-links =[]
-for ul in soup.find_all("ul"):
-    strong = ul.find('strong')
-    a      = strong.find('a')
-    link = a.attrs['href'] if "href" in a.attrs else ''
-    link = 'https://lists.debian.org/debian-devel/2018/01/' + link
-    links.append(link)
-pprint(len(links))
+links = []
+def getLinks(url, url1):
+	try :
+		html_page = urllib.request.urlopen(url + url1)
+		soup = BeautifulSoup(html_page, "lxml")
+		links1 = [a.get('href') for a in soup.find_all('a', href=True)]
+		# links = []
+		for link in links1 :
+			if link.startswith('msg') :
+				links.append(url + link)
+		print(len(links)
+	except urllib.error.HTTPError as err:
+		if err.code == 404 :
+			print()
+		else :
+			raise
+
+
+for x in range(1) :
+	month += 1
+	base_url = ''
+	if month < 10 :
+		base_url = url + str(year) + '/0' + str(month) + '/'
+	else :
+		base_url = url + str(year) + '/' + str(month) + '/'
+	# print(base_url + url1)
+	getLinks(base_url, url1)
+	for y in range (2, 5) :
+		extra_url = 'thrd' + str(y) + '.html'
+		# print(base_url + extra_url)
+		getLinks(base_url, extra_url)
+	
     
 
 # # Visit each link in the list and scrape data. Data written to file
@@ -27,13 +55,13 @@ for l in links:
     soup1 = BeautifulSoup(page1.text, 'html.parser')
     fname = str(x) + '.txt'
     file = open(fname,'w')
-    x = x+1
+    x += 1
 
     li = soup1.find_all('li')
     dict = {}
     for i in range(len(li)):
         st = li[i].text
-        s   = str(st).split(': ')
+        s  = str(st).split(': ', 2)
         if s[0] == 'Cc':
             continue
         dict[s[0]] = s[1]
