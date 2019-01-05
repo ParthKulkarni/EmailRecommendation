d5a0fac320534da893086953a9c5adbd26a4264e
niki-1905 <nikkikokitkar@gmail.com>
2019-01-05 12:01:52 +0530

    Modified preprocessing code

--- Preprocessing/preprocessing.py
+++ Preprocessing/preprocessing.py
@@ -5,160 +5,147 @@ import spacy
 
 class preprocess:
 
-    def remove_content_in_braces(self, msg):
-        msg1 = ''
-        cnt = 0
-        for char in msg :
-            if char == '{' :
-                cnt += 1
-            elif char == '}' :
-                cnt -= 1
-            elif cnt == 0 :
-                msg1 += char
-            else :
-                continue
-        return msg1
+	def remove_content_in_braces(self, msg):
+		msg1 = ''
+		cnt = 0
+		for char in msg :
+			if char == '{' :
+				cnt += 1
+			elif char == '}' :
+				cnt -= 1
+			elif cnt == 0 :
+				msg1 += char
+			else :
+				continue
+		return msg1
 
-    def remove_func_and_struct(self, msg):
-        msg1 = ''
-        take_line = True
-        msg = msg.splitlines()
-        for line in msg :
-            if line == '' :
-                continue
-            
-            temp_line = ''
-            words = line.split(' ')
-            for word in words :
-                #print(word)
-                if '.com' not in word :
-                    temp_line += word + ' ' 
-            line = temp_line
-            take_line = True
-            words = line.split(' ')
-            if words[0] == "func" :
-                take_line = False
-            elif words[0] == "type" :
-                if len(words)  >= 3  and words[2] == "struct" :
-                    take_line = False
-            if take_line :
-                msg1 += (line + '\n')
-        print(msg1)
-        return msg1
-
-    def remove_other_code_lines(self, msg) :
-        msg1 = ''
-        take_line = True
-        msg = msg.splitlines()
-        i = 0	
-        while i < len(msg) :
-            if (msg[i] == '') or ("//" in msg[i]) :
-                i += 1
-                continue
-            take_line = True
-            line = msg[i]
-            if "package" in line :
-                words = line.split(' ')
-                if len(words) < 4 :
-                    take_line = False
-            elif "import" in line :
-                words = line.split(' ')
-                if len(words) < 4 :
-                    take_line = False
-                    if "(" in line :
-                        while ')' not in msg[i] :
-                            i += 1
-            elif "const" in line :
-                words = line.split(' ')
-                if len(words) < 4 :
-                    take_line = False
-                    if "(" in line :
-                        while ')' not in msg[i] :
-                            i += 1
-            if take_line :
-                msg1 += line + '\n'
-            i += 1
-        return msg1
+	def remove_func_and_struct(self,msg):
+		msg1 = ''
+		take_line = True
+		msg = msg.splitlines()
+		for line in msg :
+			take_line = True
+			if line == '' :
+				continue
+			words = line.split(' ')
+			if words[0] == "func" :
+				take_line = False
+			elif words[0] == "type" :
+				if len(words)  >= 3  and words[2] == "struct" :
+					take_line = False
+			if take_line :
+				msg1 += (line + '\n')
+		return msg1
 
 
-    def remove_code(self, msg) :
-        msg = self.remove_content_in_braces(msg)
-        msg = self.remove_func_and_struct(msg)
-        msg = self.remove_other_code_lines(msg)
-        return msg
-                   
-    def replace_tokens(self, message):
-        message = re.sub(r"\w*.doc|\w*.pdf|\w*.txt|\w*.xls|\w*.ppt", "[filetype]", message) 
-        message = re.sub(r"\swhy|\swhere|\swho|\swhat|\swhen","[wwhh]", message)  
-        message = re.sub(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "[url]", message) 
-        message = re.sub(",|;|:|", "", message)
-        message = re.sub(r"\smonday|\smon|\stuesday|\stue|\swednesday|\swed|\sthursday|\sthu|\sfriday|\sfri|\ssaturday|\ssat|\ssunday|\ssun", "[day]", message)
-        message = re.sub(r"\sme|\sher|\shim|\sus|\sthem", "[me]", message)
-        message = re.sub(r"\sI|\swe|\syou|\she|\sshe|\sthey", "[person]", message)
-        message = re.sub(r'\d+', "[number]" ,message)
-        
-        return message
+	def remove_other_code_lines(self,msg) :
+		msg1 = ''
+		take_line = True
+		msg = msg.splitlines()
+		i = 0	
+		while i < len(msg) :
+			if (msg[i] == '') or ("//" in msg[i]) :
+				i += 1
+				continue
+			take_line = True
+			line = msg[i]
+			if "package" in line :
+				words = line.split(' ')
+				if len(words) < 4 :
+					take_line = False
+			elif "import" in line :
+				words = line.split(' ')
+				if len(words) < 4 :
+					take_line = False
+					if "(" in line :
+						while ')' not in msg[i] :
+							i += 1
+			elif "const" in line :
+				words = line.split(' ')
+				if len(words) < 4 :
+					take_line = False
+					if "(" in line :
+						while ')' not in msg[i] :
+							i += 1
+			if take_line :
+				msg1 += line + '\n'
+			i += 1
+		return msg1
 
-    def lemmatize(self, doc):
-        nlp = spacy.load("en")
-        my_stop_words = ['gmail','google','github','yahoo','com','org','http','https','golang','www','regards','thanks','html5','tidyhtml5']
-        for stopword in my_stop_words:
-            lexeme = nlp.vocab[stopword]
-            lexeme.is_stop = True
-        doc = nlp(doc)
+
+	def remove_code(self, msg) :
+		msg = self.remove_content_in_braces(msg)
+		msg = self.remove_func_and_struct(msg)
+		msg = self.remove_other_code_lines(msg)
+		return msg
+		                   
+	def replace_tokens(self, message):
+		message = re.sub(r"\w*.doc|\w*.pdf|\w*.txt|\w*.xls|\w*.ppt", "[filetype]", message) 
+		message = re.sub(r"\swhy|\swhere|\swho|\swhat|\swhen","[wwhh]", message)  
+		message = re.sub(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "[url]", message) 
+		message = re.sub(",|;|:|", "", message)
+		message = re.sub(r"\smonday|\smon|\stuesday|\stue|\swednesday|\swed|\sthursday|\sthu|\sfriday|\sfri|\ssaturday|\ssat|\ssunday|\ssun", "[day]", message)
+		message = re.sub(r"\sme|\sher|\shim|\sus|\sthem", "[me]", message)
+		message = re.sub(r"\sI|\swe|\syou|\she|\sshe|\sthey", "[person]", message)
+		message = re.sub(r'\d+', "[number]" ,message)
+
+		return message
+
+	def lemmatize(self, doc):
+		nlp = spacy.load("en")
+		my_stop_words = ['gmail','google','github','yahoo','com','org','http','https','golang','www','regards','thanks','html5','tidyhtml5']
+		for stopword in my_stop_words:
+			lexeme = nlp.vocab[stopword]
+			lexeme.is_stop = True
+		doc = nlp(doc)
         # we add some words to the stop word list
-        texts, article, skl_texts = [], [], []
-        for w in doc:
+		texts, article, skl_texts = '','',''
+		for w in doc:
             # if it's not a stop word or punctuation mark, add it to our article
-            if w.text != '\n' and not w.is_stop and not w.is_punct and not w.like_num and w.text!='I':
+			if w.text != '\n' and not w.is_stop and not w.is_punct and not w.like_num and w.text!='I':
 #               # we add the lematized version of the word
-                if w.lemma_!='-PRON-' and w.lemma_!=' \n'and w.lemma_!='\n \n \n':
-                    article.append(w.lemma_)
+				if w.lemma_!='-PRON-' and w.lemma_!=' \n'and w.lemma_!='\n \n \n':
+					article += " " + w.lemma_
             # if it's a new line, it means we're onto our next document
-            if w.text == '\n':
-                skl_texts.append(' '.join(article))
-                texts.append(article)
-                article = []
-        return skl_texts
+			if w.text == '\n':
+				skl_texts += " " + article
+				texts += " " + article
+				article = ''
+		return skl_texts
 
-    def extract_mail_body(self, msg):
-        msg = mime.from_string(msg)
-        rt = ""
-        if msg.content_type.is_singlepart():
-            temp = str(msg.body)
-            #print(temp)
-            temp = temp.splitlines()
-            for _ in temp:
-                if _.startswith('>'):
-                    continue
-                elif _.startswith('On'):
-                    continue
-                else:
-                    rt+=_
-        elif msg.content_type.is_multipart():
-            #print(msg.body)
-            for part in msg.parts :
-                print('Content-Type: {} Body: {}'.format(part, part.body))
-                #if "(text/plain)" in str(part) :
-                temp = str(part.body)
-                print(temp)
-                temp = temp.splitlines()
-                for _ in temp :
-                    if _.startswith('>') :
-                        continue
-                    if _.startswith('On'):
-                        continue
-                    else :
-                        rt+=_
-        return rt
-
+	def extract_mail_body(self, msg):
+		rt = ''
+		msg = mime.from_string(msg)
+		if msg.content_type.is_singlepart():
+			temp = str(msg.body)
+			temp = temp.splitlines()
+			for _ in temp:
+				if _.startswith('>'):
+					continue
+				elif _.startswith('On'):
+					continue
+				else:
+					rt+=_+"\n"
+		else :
+			for part in msg.parts :
+				if "(text/plain)" in str(part) :
+					temp = str(part.body)
+					temp = temp.splitlines()
+					for _ in temp :
+						if _.startswith('>') :
+						  continue
+						if _.startswith('On'):
+						  continue
+						else :
+						  rt+=_+"\n"
+		return rt
 
 
 if __name__ == '__main__':
     
     obj = preprocess()
 
-    files = glob.glob("*.email")
+    files = glob.glob("000_4Ce8YN2m-AwJ.email")
 
     bodies =[]
     for file in files:
@@ -172,11 +159,13 @@ if __name__ == '__main__':
     for body in bodies:
         nocode = obj.remove_code(body)
         #print(nocode)
-        lemm = obj.lemmatize(nocode)
-        print(lemm)
-        # replaced = obj.replace_tokens(str(lemm))
-        #print(replaced)
-        # preprocessed.append(replaced)
-    
-    # print(preprocessed)
+        lemm = obj.lemmatize(str(nocode))
+        #print(lemm)
+        replaced = obj.replace_tokens(str(lemm))
+        # print(replaced)
+        preprocessed.append(replaced)
+        # print(replaced)
+        # print('************')            
+		# print(replaced)
+    print(preprocessed)
 
